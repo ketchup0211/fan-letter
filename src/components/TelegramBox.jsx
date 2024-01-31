21dae9fe376e0b6bd78d89e512a9ea046939bbfa
ketchup0211 <dlcka1111@naver.com>
2024-01-31 19:32:08 +0900

    [MOD] message list modify

--- src/components/TelegramBox.jsx
+++ src/components/TelegramBox.jsx
@@ -6,21 +6,39 @@ const TelegramContainer = styled.div`
   margin: 0px 31.5px 10px 31.5px;
   background-color: black;
   border-radius: 10px;
-  height: 140px;
   display: flex;
+  flex-direction: row;
+  align-items: center;
 `;
 
 const ProfileImg = styled.img`
   border-radius: 360px;
-  scale: 0.65;
+  margin: 20px;
+  width: 100px;
+  height: 100px;
 `;
 
 const ContentsContainer = styled.div`
-  margin: 16px 0px;
+  padding: 5px 20px 5px 5px;
+  margin: 20px 0px;
   color: white;
   font-family: NEXON Lv2 Gothic;
-  line-height: 20px;
+  line-height: 23px;
   text-align: left;
+  min-height: 90px;
+`;
+
+const Sender = styled.p`
+  font-size: 20px;
+`;
+
+const Time = styled.p`
+  font-size: 12px;
+  color: grey;
+`;
+const Message = styled.p`
+  margin-top: 5px;
+  font-size: 16px;
 `;
 function TelegramBox({ sender, message, creationTime }) {
   return (
@@ -28,9 +46,9 @@ function TelegramBox({ sender, message, creationTime }) {
       <TelegramContainer>
         <ProfileImg src="/images/profile.jpg" alt="profile" />
         <ContentsContainer>
-          <p>{sender}</p>
-          <p>{creationTime}</p>
-          <p>{message}</p>
+          <Sender>{sender}</Sender>
+          <Time>{creationTime}</Time>
+          <Message>{message}</Message>
         </ContentsContainer>
       </TelegramContainer>
     </>
