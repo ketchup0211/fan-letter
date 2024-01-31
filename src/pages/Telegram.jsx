e8506abf52f5cb6e74d3d1827d5aeb3f097faafc
ketchup0211 <dlcka1111@naver.com>
2024-01-31 20:10:41 +0900

    [MOD] Telegram.jsx -> home으로 이동하는 버튼 구현(router)

--- src/pages/Telegram.jsx
+++ src/pages/Telegram.jsx
@@ -1,7 +1,46 @@
 import { useState } from "react";
+import styled from "styled-components";
+import "../fonts.css";
+import StBody from "../components/StBody";
+import { useNavigate } from "react-router-dom";
+const MAIN_COLOR = "#f6ebe2";
+
+const StBanner = styled.div`
+  background-image: url("public/images/chim-ha-ha.png");
+  text-align: center;
+  padding: 60px;
+  border-bottom: 3px dashed black;
+`;
+
+const GoHomeBtn = styled.button`
+  position: fixed;
+  top: 20px;
+  left: 30px;
+  background-color: white;
+  border: 2px solid black;
+  border-radius: 180px;
+  font-family: GangwonEduPowerExtraBoldA;
+  font-size: 20px;
+  padding: 10px;
+`;
 
 function Telegram() {
-  return <div>Telegram Detail Page</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <StBanner>
+        <GoHomeBtn onClick={() => navigate("/")}>홈으로</GoHomeBtn>
+        <StSubTitle>이참 님이 침착맨 님에게 보낸 전보</StSubTitle>
+      </StBanner>
+      <StBody />
+    </>
+  );
 }
 
 export default Telegram;
+
+const StSubTitle = styled.p`
+  margin-top: 6px;
+  font-family: NEXON Lv2 Gothic;
+  font-size: 20px;
+`;
