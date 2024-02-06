835e3abb77e78a7a60e66799f0e1d98daf7877cf
ketchup0211 <dlcka1111@naver.com>
2024-02-06 11:55:41 +0900

    [MOD] reset.css -> GlobalStyle.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./reset.css";
+import GlobalStyle from "./Globalstyle.jsx";
 
 import store from "./redux/config/configStore.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <GlobalStyle>
+      <App />
+    </GlobalStyle>
   </Provider>
 );
